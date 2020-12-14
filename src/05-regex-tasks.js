2905db292c5de24fa920d21da6045b11d556d63a
Volkau Aliaksey <stakebf@gmail.com>
2020-12-14 22:14:30 +0300

    feat: add solution for regex-tasks

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  throw new Error('Not implemented');
+  return /^{[0-9A-f]{8}(-[0-9A-f]{4}){3}-[0-9A-Fa-f]{12}}/;
 }
 
 
@@ -54,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-  throw new Error('Not implemented');
+  return /p.t(?!ea|ar)/;
 }
 
 
@@ -65,7 +65,7 @@ function getRegexForPitSpot() {
  *  - Contains a lowercase letter
  *  - Contains an uppercase letter
  *  - Contains a number
- *  - Valid passwords will only be alphanumeric characters (+ underscore).
+ *  - Valid passwords will only be alphanumeric characters.
  *
  * @param {number} minLength
  * @return {Regex}
@@ -78,8 +78,9 @@ function getRegexForPitSpot() {
  *   'PASSW0RD'.match(validator)  => false
  *   'Pa55'.match(validator) => false
  */
-function getPasswordValidator(/* minLength */) {
-  throw new Error('Not implemented');
+function getPasswordValidator(minLength) {
+  const reg = `(?!.*[\\s])(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z^0-9]{${minLength},}`;
+  return new RegExp(reg);
 }
 
 
